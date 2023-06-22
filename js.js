857a9ddef2ada09c31a8aa7fa32169c8ee134731
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2023-06-22 11:56:43 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -8,16 +8,21 @@ cells.forEach(cell => cell.addEventListener('click', handleCellClick));
 function handleCellClick(e) {
   const cell = e.target;
   const index = parseInt(cell.getAttribute('id').slice(-1));
-  
+
   if (board[index] !== '') return;
 
   board[index] = currentPlayer;
   cell.classList.add(currentPlayer.toLowerCase());
   cell.textContent = currentPlayer;
 
+
   if (checkWin()) {
-    alert(`${currentPlayer} Wins`);
-    resetBoard();
+    setTimeout(function () {
+      alert(`${currentPlayer} Wins`);
+    }, 300);
+    setTimeout(function () {
+      resetBoard();
+    }, 500);
     return;
   }
 
@@ -26,8 +31,8 @@ function handleCellClick(e) {
     resetBoard();
     return;
   }
-
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
 }
 
 function checkWin() {
